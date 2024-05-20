8238f77f6dbd1c293162a62b08dcbefe65ef91f1
SHANJIDA-HRIDE <shanjidahride1997@gmail.com>
2024-05-20 16:14:37 +0600

    manual entry test

--- src/test/java/pages/VehicleManualEntryPage.java
+++ src/test/java/pages/VehicleManualEntryPage.java
@@ -7,6 +7,6 @@ public class VehicleManualEntryPage extends basePage{
 
     public By clickSubmitVehicleManualEntryCard = By.xpath("//button[@class='btn btn-primary mr-2']");
     public By enterVehicleManualEntryCard = By.id("card_number");
-
+    public By vehicleManualEntrySuccessAssertion = By.xpath("//div[@class='alert alert-success alert-dismissible fade show' and @role='alert']//b\n");
 
 }
--- src/test/java/testcases/VehicleManualEntryTest.java
+++ src/test/java/testcases/VehicleManualEntryTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import pages.CardIssuePage;
 import pages.DashboardPage;
 import pages.LoginPage;
+import pages.VehicleManualEntryPage;
 import utilities.DriverSetup;
 
 public class VehicleManualEntryTest extends DriverSetup {
@@ -13,31 +14,24 @@ public class VehicleManualEntryTest extends DriverSetup {
     DashboardPage dashboardPage = new DashboardPage();
     CardIssuePage cardIssuePage = new CardIssuePage();
     LoginPage loginPage = new LoginPage();
+    VehicleManualEntryPage vehicleManualEntry = new VehicleManualEntryPage();
+
     @Test(priority = 1)
-    @Description("Validate Entry of Card Issue with Mode Post-Paid Hourly")
-    public void cardIssueWithModePostPaidHourly() throws InterruptedException {
+    @Description("Validate Entry of Vehicle Manual Entry with Card")
+    public void VehicleManualEntry() throws InterruptedException {
         getDriver().get(loginPage.loginPageUrl);
         loginPage.writeOnElement(loginPage.Username,"tausif");
         loginPage.writeOnElement(loginPage.Password,"1234");
         loginPage.clickOnElement(loginPage.loginButton);
         Thread.sleep(2000);
-        getDriver().get(cardIssuePage.cardIssuePageUrl);
-        cardIssuePage.addScreenShot("After click to the Card Issue page");
-        Assert.assertEquals(cardIssuePage.getElementText(cardIssuePage.cardIssuePageAssertion),"Card Issue at Entry Booth");
-        cardIssuePage.selectWithVisibleText(cardIssuePage.selectCardMode,"Post-Paid");
+        getDriver().get(vehicleManualEntry.vehicleManualEntryPageUrl);
+        vehicleManualEntry.writeOnElement(vehicleManualEntry.enterVehicleManualEntryCard,"Hride4");
         Thread.sleep(3000);
-        cardIssuePage.selectWithVisibleText(cardIssuePage.selectCardTariffType,"Hourly");
-        cardIssuePage.selectWithVisibleText(cardIssuePage.selectCardVehicleType,"Motorcycle");
-        Thread.sleep(3000);
-        cardIssuePage.selectWithVisibleText(cardIssuePage.selectCard,"Hride2");
-        Thread.sleep(3000);
-        cardIssuePage.writeOnElement(cardIssuePage.enterDriverName,"Hafiz");
-        cardIssuePage.writeOnElement(cardIssuePage.enterVehicleRegistrationNumber,"Fa-41-1000");
-        cardIssuePage.writeOnElement(cardIssuePage.enterDriverContact,"01777048151");
-        cardIssuePage.clickOnElement(cardIssuePage.clickCardSubmitButton);
-        cardIssuePage.addScreenShot("After Successful Card Issue");
-        Assert.assertEquals(cardIssuePage.getElementText(cardIssuePage.cardEnterSuccessAssertion),"The card issued successfully.");
+        vehicleManualEntry.clickOnElement(vehicleManualEntry.clickSubmitVehicleManualEntryCard);
+        vehicleManualEntry.addScreenShot("After Successful Vehicle Manual Entry");
+        Assert.assertEquals(vehicleManualEntry.getElementText(vehicleManualEntry.vehicleManualEntrySuccessAssertion),"Manual parking entry created successfully.");
     }
+
 
 
 }
